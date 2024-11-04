31e0eeac0aaf6f703870848d4f5ded01507e4a06
Tijm van Dongen <i486333@Fhict.nl>
2024-11-04 14:38:10 +0100

    initial commit

---
+++ src/main/java/com/sjtzooi/templatelibrary_nosql/TemplatelibraryNoSqlApplication.java
@@ -0,0 +1,13 @@
+package com.sjtzooi.templatelibrary_nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatelibraryNoSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplatelibraryNoSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sjtzooi/templatelibrary_nosql/TemplatelibraryNoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjtzooi.templatelibrary_nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplatelibraryNoSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
